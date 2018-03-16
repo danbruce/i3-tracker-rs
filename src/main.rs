fbdb6593f28b70b226b5499726d1770b90b217f3
Daniel Bruce <dbruce1126@gmail.com>
2018-03-16 13:30:38 -0400

    Refactors the struct away.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,5 @@ extern crate xcb;
 mod time_tracker;
 
 fn main() {
-    let mut tracker = time_tracker::TimeTracker::new();
-    tracker.run();
+    time_tracker::track_time();
 }
--- src/time_tracker.rs
+++ src/time_tracker.rs
@@ -4,85 +4,72 @@ use i3ipc::event::Event;
 use i3ipc::event::inner::WindowChange;
 use xcb;
 
-pub struct TimeTracker {
-    i3_listener: I3EventListener,
-    xorg_conn: xcb::Connection,
-}
-
-impl TimeTracker {
-    pub fn new() -> Self {
-        let i3_listener = I3EventListener::connect().unwrap();
-        let (xorg_conn, _screen_num) = xcb::Connection::connect(None).unwrap();
-        TimeTracker {
-            i3_listener,
-            xorg_conn,
-        }
-    }
-    pub fn run(&mut self) {
-        let subs = [Subscription::Window];
-        self.i3_listener.subscribe(&subs).unwrap();
-
-        // handle them
-        for event in self.i3_listener.listen() {
-            match event.unwrap() {
-                Event::WindowEvent(e) => match e.change {
-                    WindowChange::Focus => match e.container.name {
-                        Some(n) => println!(
-                            "{} ({})",
-                            n,
-                            self.get_class(&e.container.window.unwrap())
-                        ),
-                        None => println!(
-                            "Untitled ({})",
-                            self.get_class(&e.container.window.unwrap())
-                        ),
-                    },
-                    _ => {}
-                },
-                _ => {}
-            }
-        }
-    }
-    /**
-    * pulled from:
-    * https://stackoverflow.com/questions/44833160/how-do-i-get-the-x-window-class-given-a-window-id-with-rust-xcb
-    */
-    fn get_class(&self, id: &i32) -> String {
-        let conn = self.xorg_conn
-        let window: xcb::xproto::Window = *id as u32;
-        let long_length: u32 = 8;
-        let mut long_offset: u32 = 0;
-        let mut buf = Vec::new();
-        loop {
-            let cookie = xcb::xproto::get_property(
-                &conn,
-                false,
-                window,
-                xcb::xproto::ATOM_WM_CLASS,
-                xcb::xproto::ATOM_STRING,
-                long_offset,
-                long_length,
-            );
-            match cookie.get_reply() {
-                Ok(reply) => {
-                    let value: &[u8] = reply.value();
-                    buf.extend_from_slice(value);
-                    match reply.bytes_after() {
-                        0 => break,
-                        _ => {
-                            let len = reply.value_len();
-                            long_offset += len / 4;
-                        }
+/**
+ * pulled from:
+ * https://stackoverflow.com/questions/44833160/how-do-i-get-the-x-window-class-given-a-window-id-with-rust-xcb
+ */
+fn get_class(conn: &xcb::Connection, id: &i32) -> String {
+    let window: xcb::xproto::Window = *id as u32;
+    let long_length: u32 = 8;
+    let mut long_offset: u32 = 0;
+    let mut buf = Vec::new();
+    loop {
+        let cookie = xcb::xproto::get_property(
+            conn,
+            false,
+            window,
+            xcb::xproto::ATOM_WM_CLASS,
+            xcb::xproto::ATOM_STRING,
+            long_offset,
+            long_length,
+        );
+        match cookie.get_reply() {
+            Ok(reply) => {
+                let value: &[u8] = reply.value();
+                buf.extend_from_slice(value);
+                match reply.bytes_after() {
+                    0 => break,
+                    _ => {
+                        let len = reply.value_len();
+                        long_offset += len / 4;
                     }
                 }
-                Err(err) => {
-                    println!("{:?}", err);
-                    break;
-                }
+            }
+            Err(err) => {
+                println!("{:?}", err);
+                break;
             }
         }
-        let result = String::from_utf8(buf).unwrap();
-        let results: Vec<&str> = result.split('\0').collect();
-        results[0].to_string()
+    }
+    let result = String::from_utf8(buf).unwrap();
+    let results: Vec<&str> = result.split('\0').collect();
+    results[0].to_string()
+}
+
+pub fn track_time() {
+    let mut i3_listener = I3EventListener::connect().unwrap();
+    let (xorg_conn, _screen_num) = xcb::Connection::connect(None).unwrap();
+
+    let subs = [Subscription::Window];
+    i3_listener.subscribe(&subs).unwrap();
+    // handle them
+    for event in i3_listener.listen() {
+        match event.unwrap() {
+            Event::WindowEvent(e) => match e.change {
+                WindowChange::Focus => match e.container.name {
+                    Some(n) => println!(
+                        "{} ({})",
+                        n,
+                        get_class(&xorg_conn, &e.container.window.unwrap())
+                    ),
+                    None => println!(
+                        "Untitled ({})",
+                        get_class(&xorg_conn, &e.container.window.unwrap())
+                    ),
+                },
+                _ => {}
+            },
+            _ => {}
+        }
     }
 }
