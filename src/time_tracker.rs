147b984059a8c7a37f888f8f851b012c9a26e034
Evan Cameron <evan@vectorface.com>
2018-03-20 12:42:09 -0400

    add serde, remove file append

--- src/time_tracker.rs
+++ src/time_tracker.rs
@@ -3,13 +3,14 @@ use csv::{Reader, Writer};
 use i3ipc::I3EventListener;
 use i3ipc::Subscription;
 use i3ipc::event::Event;
-use i3ipc::event::inner::WindowChange;
 use i3ipc::event::WindowEventInfo;
+use i3ipc::event::inner::WindowChange;
 use std::error::Error;
 use std::fs::{File, OpenOptions};
+use std::path::Path;
 use xcb;
 
-/**
+/*
  * pulled from:
  * https://stackoverflow.com/questions/44833160/how-do-i-get-the-x-window-class-given-a-window-id-with-rust-xcb
  */
@@ -46,27 +47,19 @@ fn get_class(conn: &xcb::Connection, id: &i32) -> String {
         }
     }
     let result = String::from_utf8(buf).unwrap();
-    let results: Vec<&str> = result.split('\0').collect();
+    let results: Vec<_> = result.split('\0').collect();
     results[0].to_string()
+    //         String::from_utf8(buf)
+    //         .unwrap()
+    //         .split('\0')
+    //         .take(1)
+    //         .next()
+    //         .unwrap()
+    //         .to_string()
 }
 
-fn write_event_to_file(writer: &mut Writer<File>, e: &LogEvent) -> Result<(), Box<Error>> {
-    let row = e.get_output_row()?;
-    writer.write_record(&row)?;
-    writer.flush()?;
-    Ok(())
-}
-fn write_header_to_file(writer: &mut Writer<File>) -> Result<(), Box<Error>> {
-    let header: OutputRow = [
-        "id".to_string(),
-        "window_id".to_string(),
-        "window_title".to_string(),
-        "window_class".to_string(),
-        "start_time".to_string(),
-        "end_time".to_string(),
-        "duration".to_string(),
-    ];
-    writer.write_record(&header)?;
+fn write_event_to_file(writer: &mut Writer<File>, log: &Log) -> Result<(), Box<Error>> {
+    writer.serialize(log)?;
     writer.flush()?;
     Ok(())
 }
@@ -80,54 +73,38 @@ fn next_event(
         Some(w) => w,
         None => -1,
     };
-    let window_title: String = match &event.container.name {
-        &Some(ref s) => s.clone(),
-        &None => "Untitled".to_string(),
-    };
     *next_event_id += 1;
+
     LogEvent {
         id: *next_event_id - 1,
-        start_date_time: Local::now(),
-        window_id: window_id,
+        start_time: Local::now(),
+        window_id,
         window_class: get_class(&xorg_conn, &window_id),
-        window_title: window_title,
+        window_title: event.container.name.clone().unwrap_or("Untitled".into()),
     }
 }
 
-fn get_next_event_id(output_filename: &str) -> u32 {
+fn next_event_id<P: AsRef<Path>>(output_filename: P) -> Result<u32, Box<Error>> {
     if let Ok(f) = OpenOptions::new().read(true).open(output_filename) {
         let mut r = Reader::from_reader(f);
-        if let Some(Ok(last_line)) = r.into_records().last() {
-            if last_line.len() > 0 {
-                if let Ok(i) = last_line[0].parse::<u32>() {
-                    return i;
-                }
-            }
+        if let Some(res) = r.deserialize().last() {
+            let log: Log = res?;
+            return Ok(log.id);
         }
     }
-    1
+    Ok(1)
 }
 
-fn get_csv_writer(output_filename: &str, has_lines: bool) -> Result<Writer<File>, Box<Error>> {
-    if let Ok(append) = OpenOptions::new().append(true).open(output_filename) {
-        if has_lines {
-            return Ok(Writer::from_writer(append));
-        }
-    }
-    let file = OpenOptions::new()
-        .create(true)
-        .write(true)
-        .open(output_filename)?;
-    let mut writer = Writer::from_writer(file);
-    write_header_to_file(&mut writer)?;
-    Ok(writer)
+fn csv_writer<P: AsRef<Path>>(path: P) -> Result<Writer<File>, Box<Error>> {
+    let file = OpenOptions::new().create(true).append(true).open(path)?;
+    Ok(Writer::from_writer(file))
 }
 
-pub fn track_time(output_filename: &str) -> Result<(), Box<Error>> {
+pub fn track_time<P: AsRef<Path>>(out_path: P) -> Result<(), Box<Error>> {
     let mut i3_listener = I3EventListener::connect()?;
     let (xorg_conn, _screen_num) = xcb::Connection::connect(None)?;
-    let mut next_event_id = get_next_event_id(output_filename);
-    let mut writer = get_csv_writer(output_filename, next_event_id > 1)?;
+    let mut next_event_id = next_event_id(&out_path)?;
+    let mut writer = csv_writer(&out_path)?;
 
     let subs = [Subscription::Window];
     i3_listener.subscribe(&subs)?;
@@ -146,14 +123,14 @@ pub fn track_time(output_filename: &str) -> Result<(), Box<Error>> {
                             continue;
                         }
                         if let Some(e) = current_event {
-                            write_event_to_file(&mut writer, &e)?;
+                            write_event_to_file(&mut writer, &Log::new(e))?;
                         }
                         current_event = Some(next_event(&mut next_event_id, &e, &xorg_conn));
                     }
                     WindowChange::Title => {
                         last_event_new = false;
                         if let Some(e) = current_event {
-                            write_event_to_file(&mut writer, &e)?;
+                            write_event_to_file(&mut writer, &Log::new(e))?;
                         }
                         current_event = Some(next_event(&mut next_event_id, &e, &xorg_conn));
                     }
@@ -166,28 +143,37 @@ pub fn track_time(output_filename: &str) -> Result<(), Box<Error>> {
     Ok(())
 }
 
-type OutputRow = [String; 7];
-
 struct LogEvent {
     id: u32,
-    start_date_time: DateTime<Local>,
+    start_time: DateTime<Local>,
     window_id: i32,
     window_class: String,
     window_title: String,
 }
 
-impl LogEvent {
-    fn get_output_row(&self) -> Result<OutputRow, Box<Error>> {
+#[derive(Debug, Deserialize, Serialize)]
+struct Log {
+    id: u32,
+    start_time: String,
+    end_time: String,
+    duration: i64,
+    window_id: i32,
+    window_class: String,
+    window_title: String,
+}
+
+impl Log {
+    fn new(event: LogEvent) -> Log {
         let now = Local::now();
-        let elapsed = now.signed_duration_since(self.start_date_time);
-        Ok([
-            self.id.to_string(),
-            self.window_id.to_string(),
-            self.window_title.clone(),
-            self.window_class.clone(),
-            self.start_date_time.format("%F %T").to_string(),
-            now.format("%F %T").to_string(),
-            elapsed.num_seconds().to_string(),
-        ])
+        let elapsed = now.signed_duration_since(event.start_time);
+        Log {
+            id: event.id,
+            window_id: event.window_id,
+            window_class: event.window_class,
+            window_title: event.window_title,
+            duration: elapsed.num_seconds(),
+            start_time: event.start_time.format("%F %T").to_string(),
+            end_time: now.format("%F %T").to_string(),
+        }
     }
 }
